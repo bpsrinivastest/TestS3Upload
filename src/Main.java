5b1dc8d0e62bade2b1e03ca301de39940c428b93
BennabPS <BennabPS@FTL1860244C7E5E.cdhs.state.co.us>
2019-02-10 16:57:17 -0700

    Added 2nd file to upload

--- src/Main.java
+++ src/Main.java
@@ -58,7 +58,7 @@ public class Main {
             //String filePath = "src\\AnypointStudio.zip";
             //String keyName = "AnypointStudio.zip";
 
-	        String filePath = "bin\\MyStudentData1.txt";
+	        String filePath = "bin\\MyStudentData2.txt";
             String keyName = "MyStudentData1.txt";
 
             File file = new File(filePath);
